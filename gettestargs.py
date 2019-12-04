bfa650c4bcc20f473f132ce6a2fb7bbf07559de0
Nils Hölscher <nilhoel1@gmail.com>
2019-12-04 13:31:18 +0100

    added threading test arg.

--- gettestargs.py
+++ gettestargs.py
@@ -5,5 +5,7 @@ def gettestargs():
     parser.add_argument("-f", "--filter", help="Use file as filter")
     parser.add_argument("-p", "--plot", help="Visualizing the process of RAISR image upscaling", action="store_true")
     parser.add_argument("-m", "--movie", help="Use video files")
+    parser.add_argument("-g", "--gpgpu", help="Accelerate by using GPU")
+    parser.add_argument("-t", "--threaded", help="Use multiple threads to train")
     args = parser.parse_args()
     return args
